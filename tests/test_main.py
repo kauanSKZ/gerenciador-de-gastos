589e3459b3901efd7cb67be1131b6e24e9ca428a
Kauan Rodrigues da Silva <kauann09112006@gotmail.com>
2026-04-11 14:44:39 -0300

    Ajuste das linhas em branco na linha 15

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,19 +14,16 @@ def gerenciar_arquivo_teste():
         os.remove("test_gastos.json")
 
 
-
 def test_caminho_feliz_adicionar_gasto():
     """Teste 1: Adição correta de um gasto."""
     assert app.adicionar_gasto("Internet", 100.0) is True
     assert len(app.carregar_gastos()) == 1
 
 
-
 def test_entrada_invalida_valor_negativo():
     """Teste 2: Validação de erro para valor negativo."""
     with pytest.raises(ValueError, match="O valor deve ser maior que zero"):
         app.adicionar_gasto("Erro", -50.0)
-
 
 
 def test_caso_limite_soma_vazia():
